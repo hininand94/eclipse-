d153833ad68d66a9e3667153aeceaa4864b8fe33
hininand94 <hininand94@gmail.com>
2026-09-09 15:17:15 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,11 @@
+package chapter;
+
+public class variables {
+
+public static void main(string[]) {
+	int$price=50000;
+	String Product="Phone";
+	System.outprintln("$price:"+$price);
+	System.outprintln("$Phone:"+$phone);
+    }
+}
